15b9806ebc06294e9b1a34ef877bc3686a57f417
YourmixJNR <radmakson@gmail.com>
2025-06-13 04:52:09 +0100

    feat: add ERC20Mock contract and update tests for minting functionality

--- test/ERC20.ts
+++ test/ERC20.ts
@@ -4,8 +4,11 @@ describe("ERC20", function () {
   it("transfer tokens correctly", async function () {
     const [alice, bob] = await ethers.getSigners();
 
-    const ERC20 = await ethers.getContractFactory("ERC20");
+    const ERC20 = await ethers.getContractFactory("ERC20Mock");
     const erc20Token = await ERC20.deploy("Yourmix", "YOM", 18);
+
+    await erc20Token.mint(alice.address, 300);
+
     await erc20Token.transfer(bob.address, 100);
   });
 });
