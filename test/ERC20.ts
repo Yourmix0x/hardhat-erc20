674b72dc000dd86a442feb57b9c204d266cf7303
Michael Victor <radmakson@gmail.com>
2025-07-04 09:49:04 +0100

    test: add additional tests for ERC20 transfer functionality and event emission

--- test/ERC20.ts
+++ test/ERC20.ts
@@ -22,9 +22,35 @@ describe("ERC20", function () {
       [-100, 100]
     );
 
-    
     expect(
       await erc20Token.connect(bob).transfer(alice.address, 50)
     ).to.changeTokenBalance(erc20Token, [alice, bob], [50, -50]);
+  });
+
+  it("should revert if sender has insufficient balance", async function () {
+    const [alice, bob] = await ethers.getSigners();
+
+    const ERC20 = await ethers.getContractFactory("ERC20Mock");
+    const erc20Token = await ERC20.deploy("Yourmix", "YOM", 18);
+
+    await erc20Token.mint(alice.address, 300);
+
+    await expect(erc20Token.transfer(bob.address, 400)).to.be.revertedWith(
+      "ERC20: transfer amount exceeds balance"
+    );
+  });
+
+  it.only("should emit Transfer event on transfers", async function () {
+    const [alice, bob] = await ethers.getSigners();
+
+    const ERC20 = await ethers.getContractFactory("ERC20Mock");
+    const erc20Token = await ERC20.deploy("Yourmix", "YOM", 18);
+
+    await erc20Token.mint(alice.address, 300);
+
+    await expect(erc20Token.transfer(bob.address, 200)).to.emit(
+      erc20Token,
+      "Transfer"
+    );
   });
 });
