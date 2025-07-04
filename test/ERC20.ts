efd44e3eda7ea85aa2e69500b43218d2322db4a6
Michael Victor <radmakson@gmail.com>
2025-07-04 08:10:07 +0100

    test: enhance ERC20 transfer test with balance assertions

--- test/ERC20.ts
+++ test/ERC20.ts
@@ -1,4 +1,5 @@
 import { ethers } from "hardhat";
+import { expect } from "chai";
 
 describe("ERC20", function () {
   it("transfer tokens correctly", async function () {
@@ -10,5 +11,11 @@ describe("ERC20", function () {
     await erc20Token.mint(alice.address, 300);
 
     await erc20Token.transfer(bob.address, 100);
+
+    const aliceBalance = await erc20Token.balanceOf(alice.address);
+    const bobBalance = await erc20Token.balanceOf(bob.address);
+
+    expect(aliceBalance).to.equals(200);
+    expect(bobBalance).to.equals(100);
   });
 });
